2b6f57e97770364a833d3ae88cbf27d436b8d130
NOVABASE\NB22276 <joao.fernandes.pinheiro@celfocus.com>
2019-12-16 17:03:40 +0000

    Config Base de Bados adicionada

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
+const express = require("express");
+const cors = require("cors");
 const APIs = require('./getAPI')
 const request = require('./createRequest')
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+const app = express();
+app.use(cors());
 
 
-// PORT 3000 CREATE DATABASE
-server.listen(() => {
-    const port = 3000;
-    console.log(`Server running at http://${hostname}:${port}/`);
-   
-    //Get All Countries
-    //APIs.get(request.createRequest('http://www.api-football.com/demo/api/v2/'));
+//PORT 3000 CREATE DATABASE
+//Create the Connection to the Database
+app.get("/createdatabase", (request, response) => {
+  var db = require('./Database/Relations')
+  const connection = require('./Database/Config');
+  connection.sync({force: true})
+})
+
+
+  //Get All Countries
+  //APIs.get(request.createRequest('http://www.api-football.com/demo/api/v2/'));
     
-    //Get all Leagues
-    //APIs.get(request.createRequest('https://api-football-v1.p.rapidapi.com/v2/leagues/'));
+  //Get all Leagues
+  //APIs.get(request.createRequest('https://api-football-v1.p.rapidapi.com/v2/leagues/'));
 
-    //Get all 
+  //Get all 
 
-    //Get all bookmarkers
-    //APIs.get(APIs.createRequest("https://api-football-v1.p.rapidapi.com/v2/odds/bookmakers/"));
+  //Get all bookmarkers
+  //APIs.get(APIs.createRequest("https://api-football-v1.p.rapidapi.com/v2/odds/bookmakers/"));
 
-    //Get all labels
-    //APIs.get(APIs.createRequest("https://api-football-v1.p.rapidapi.com/v2/odds/labels"));
+  //Get all labels
+  //APIs.get(APIs.createRequest("https://api-football-v1.p.rapidapi.com/v2/odds/labels"));
 
+// Select Listen Port
 
-
-});
+app.listen(8000, () => {console.log("Listening on port 8000");});
 
